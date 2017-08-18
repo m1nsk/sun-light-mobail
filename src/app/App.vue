c0194a068d1ebbe1a92a10fd0dc00179588e4c99
Alex Soveliev <Suroviy@bk.ru>
2017-08-18 20:49:57 +0300

    Добавил animate.css

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="page">
-    <transition name="slide">
+    <transition
+    name="custom-classes-transition"
+    enter-active-class="animated bounceInRight"
+    leave-active-class="animated bounceOutRight"
+  >
       <router-view></router-view>
     </transition>
     <menu-footer @view="changeView" :menuItemData="menuItemData"></menu-footer>
@@ -58,17 +62,5 @@
     font-family: 'futurabookcregular',Arial,Helvetica,sans-serif;
   }
 
-  .slide-enter-active {
-    transition: all .3s ease;
-  }
-  /*
-  .slide-leave-active {
-    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-  }
-  */
-  .slide-enter, .slide-leave-to
-  /* .slide-fade-leave-active до версии 2.1.8 */ {
-    transform: translateX(300px);
-    opacity: 0.5;
-  }
+@import './animate.min.less';
 </style>
